8d4d0a662ed970893df4f64e674886ccd8839c46
Wilfredo Camacho Sánchez <wilfredo@jointre.net>
2020-07-14 14:06:55 -0400

    tst

--- js/csv_to_html_table.js
+++ js/csv_to_html_table.js
@@ -37,7 +37,7 @@ CsvToHtmlTable = {
                 for (var rowIdx = 1; rowIdx < csvData.length; rowIdx++) {
                     var $tableBodyRow = $("<tr></tr>");
                     for (var colIdx = 0; colIdx < csvData[rowIdx].length; colIdx++) {
-                        var $tableBodyRowTd = $("<td></td>");
+                        var $tableBodyRowTd = $("<td>""</td>");
                         var cellTemplateFunc = customTemplates[colIdx];
                         if (cellTemplateFunc) {
                             $tableBodyRowTd.html(cellTemplateFunc(csvData[rowIdx][colIdx]));
